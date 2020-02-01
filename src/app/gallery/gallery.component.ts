14658b6c38a14f1cdd6536a17dc48b22c8a984a3
Ruslan <ruslan.ogarkov88@gmail.com>
2020-02-01 20:49:31 +0200

    add siders to the gallery page

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,15 +1,30 @@
+/** @format */
+
 import { Component, OnInit } from '@angular/core';
+import Flickity from 'flickity';
 
 @Component({
-  selector: 'app-gallery',
-  templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+	selector: 'app-gallery',
+	templateUrl: './gallery.component.html',
+	styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	ngOnInit() {
+		// external js: flickity.pkgd.js
 
-  ngOnInit() {
-  }
+		var flkty = new Flickity('.carousel', {
+			fade: true
+		});
+		var carouselStatus = document.querySelector('.carousel-status');
 
+		function updateStatus() {
+			var slideNumber = flkty.selectedIndex + 1;
+			carouselStatus.textContent = slideNumber + '/' + flkty.slides.length;
+		}
+		updateStatus();
+
+		flkty.on('select', updateStatus);
+	}
 }
